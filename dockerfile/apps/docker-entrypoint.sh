44c81f0108b01675e7f6fd35aece6121eced3248
anthonychen <anthonychen@lexin.com>
2020-07-07 15:16:53 +0800

    jdk dockerfile

--- dockerfile/apps/docker-entrypoint.sh
+++ dockerfile/apps/docker-entrypoint.sh
@@ -9,10 +9,10 @@ fi
 
 if [ "$(ls -A ${CONSUL_CONFIG_DIR_PARAM})" = "" ]; then
   echo 'consul配置目录为空, dev模式启动'
-  nohub consul -dev > /dev/null 2>&1
+  consul -dev > /dev/null 2>&1
 else
   echo "consul配置目录: ${CONSUL_CONFIG_DIR_PARAM}"
-  nohub consul agent -config-dir=${CONSUL_CONFIG_DIR_PARAM} > /dev/null 2>&1
+  consul agent -config-dir=${CONSUL_CONFIG_DIR_PARAM} > /dev/null 2>&1
 fi
 
 echo '启动java'
