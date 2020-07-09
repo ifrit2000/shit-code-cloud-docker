baf6c3c0200bc3c921fd0fb5c7d2658ca213330a
Anthony <cdistc@163.com>
2020-07-09 11:15:42 +0800

    docker

---
+++ dockerfile/apps/base.Dockerfile
@@ -0,0 +1,10 @@
+FROM gradle:6.5.1-jdk11
+MAINTAINER Anthony
+ARG GIT_POSTFIX
+ARG GIT_BRANCH
+RUN mkdir -p /tmp/build &&\
+    cd /tmp/build &&\
+    git clone https://github.com/cd871127/shit-code-cloud-${GIT_POSTFIX}.git &&\
+    cd shit-code-cloud-${GIT_POSTFIX} &&\
+    git checkout -b ${GIT_BRANCH} origin/${GIT_BRANCH}  &&\
+    gradle bootJar
--- dockerfile/apps/build.sh
+++ dockerfile/apps/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-NAME=gateway
-
-docker build  --no-cache --build-arg GIT_BRANCH=dev --build-arg APP_NAME=infrastructure-${NAME} \
-  --build-arg GIT_REPO=shit-code-cloud-infrastructure -f app.Dockerfile -t cd871127/infrastructure-${NAME}:dev .
-
-NAME=admin
-docker build  --no-cache --build-arg GIT_BRANCH=dev --build-arg APP_NAME=infrastructure-${NAME} \
-  --build-arg GIT_REPO=shit-code-cloud-infrastructure -f app.Dockerfile -t cd871127/infrastructure-${NAME}:dev .
-#docker build  --no-cache --build-arg GIT_BRANCH=dev --build-arg APP_NAME=infrastructure-admin \
-#  --build-arg GIT_REPO=shit-code-cloud-infrastructure -f app.Dockerfile -t cd871127/infrastructure-admin:dev .
+app_name=("1" "2")
+#echo ${app_name[0]}
+#docker build  --no-cache --build-arg GIT_BRANCH=dev --build-arg APP_NAME=infrastructure-${NAME} \
+#  --build-arg GIT_REPO=shit-code-cloud-infrastructure -f app.Dockerfile -t cd871127/infrastructure-${NAME}:dev .
+for var in ${app_name[*]}; do
+  echo "${var}"
+done
