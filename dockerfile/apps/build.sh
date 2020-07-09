10a28856c30cf073dadc210814fe3303010499f4
anthonychen <anthonychen@lexin.com>
2020-07-09 11:32:48 +0800

    docker

--- dockerfile/apps/build.sh
+++ dockerfile/apps/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#echo ${app_name[0]}
-#docker build  --no-cache --build-arg GIT_BRANCH=dev --build-arg APP_NAME=infrastructure-${NAME} \
-#  --build-arg GIT_REPO=shit-code-cloud-infrastructure -f app.Dockerfile -t cd871127/infrastructure-${NAME}:dev .
-
 # 构建基础jar镜像
 GIT_POSTFIX=infrastructure
 GIT_BRANCH=dev
-APP_NAME=gateway
-JAR_NAME=${GIT_POSTFIX}-${APP_NAME}
+APP_NAMES=(gateway admin)
+TMP_IMAGE=app-base:tmp
 
 docker build --build-arg GIT_BRANCH=${GIT_BRANCH} --build-arg GIT_POSTFIX=${GIT_POSTFIX} -f base.Dockerfile -t app-base:tmp .
 
-docker build --build-arg APP_NAME=${APP_NAME} --build-arg GIT_POSTFIX=${GIT_POSTFIX} -f app.Dockerfile -t cd871127/${APP_NAME}:dev .
+for APP_NAME in ${APP_NAMES[*]}; do
+  JAR_NAME=${GIT_POSTFIX}-${APP_NAME}
+  docker build --build-arg APP_NAME=${APP_NAME} --build-arg GIT_POSTFIX=${GIT_POSTFIX} -f app.Dockerfile -t cd871127/${JAR_NAME}:dev .
+done
+docker rmi ${TMP_IMAGE}
