fc423e78ebecfce040c893e4a690cea86ad43e31
anthonychen <anthonychen@lexin.com>
2020-07-09 17:47:31 +0800

    jdk dockerfile

--- dockerfile/apps/docker-entrypoint.sh
+++ dockerfile/apps/docker-entrypoint.sh
@@ -7,12 +7,16 @@ else
   CONSUL_CONFIG_DIR_PARAM=${CONSUL_CONFIG_DIR}
 fi
 
-if [ "$(ls -A ${CONSUL_CONFIG_DIR_PARAM})" = "" ]; then
-  echo 'consul配置目录为空, dev模式启动'
-  nohup consul agent -dev -bind=0.0.0.0 -client=0.0.0.0 > /consul/data/1 2>&1 &
-else
-  echo "consul配置目录: ${CONSUL_CONFIG_DIR_PARAM}"
-  nohup consul agent -config-dir=${CONSUL_CONFIG_DIR_PARAM} > /dev/null 2>&1 &
+if [ ! ${PROFILE} ]; then
+  PROFILE=cluster
 fi
 
-java -Xmx512m -Xms512m -Xmn192m -jar ${JAR_NAME}.jar --spring.profiles.active=cluster -xmx
+if [ "$(ls -A ${CONSUL_CONFIG_DIR_PARAM})" = "" ]; then
+  echo 'consul配置目录为空, dev模式启动'
+  nohup consul agent -dev -bind=0.0.0.0 -client=0.0.0.0 >/consul/data/1 2>&1 &
+else
+  echo "consul配置目录: ${CONSUL_CONFIG_DIR_PARAM}"
+  nohup consul agent -config-dir=${CONSUL_CONFIG_DIR_PARAM} >/dev/null 2>&1 &
+fi
+
+java -Xmx512m -Xms512m -Xmn192m -jar ${JAR_NAME}.jar --spring.profiles.active=${PROFILE} -xmx
