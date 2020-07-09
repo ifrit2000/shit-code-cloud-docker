d5fee0a9153fb2ef1fc9cc48de29b006152c21ef
anthonychen <anthonychen@lexin.com>
2020-07-09 11:25:11 +0800

    docker

--- dockerfile/apps/build.sh
+++ dockerfile/apps/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-app_name=("1" "2")
 #echo ${app_name[0]}
 #docker build  --no-cache --build-arg GIT_BRANCH=dev --build-arg APP_NAME=infrastructure-${NAME} \
 #  --build-arg GIT_REPO=shit-code-cloud-infrastructure -f app.Dockerfile -t cd871127/infrastructure-${NAME}:dev .
@@ -8,9 +7,8 @@ app_name=("1" "2")
 GIT_POSTFIX=infrastructure
 GIT_BRANCH=dev
 
-APP_NAME=admin
+APP_NAME=infrastructure-admin
 
 docker build --build-arg GIT_BRANCH=${GIT_BRANCH} --build-arg GIT_POSTFIX=${GIT_POSTFIX} -f base.Dockerfile -t app-base:tmp .
 
-
-docker build --build-arg APP_NAME=${APP_NAME} --build-arg GIT_POSTFIX=${GIT_POSTFIX} -f base.Dockerfile -t app-base:tmp .
+docker build --build-arg APP_NAME=${APP_NAME} --build-arg GIT_POSTFIX=${GIT_POSTFIX} -f app.Dockerfile -t ${APP_NAME}:tmp .
