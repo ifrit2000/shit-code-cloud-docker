ccf201c93588b1f44d2f427f1696e25284e94df2
anthonychen <anthonychen@lexin.com>
2020-10-13 13:34:28 +0800

    script

--- docker-compose/cluster/script/start.sh
+++ docker-compose/cluster/script/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd ../compose-file
 docker-compose \
--f 01_network.yml \
+-f 00_network.yml \
+-f 01_haproxy.yml \
 -f 02_consul.yml \
 -f 03_mysql.yml \
 -f 04_redis.yml \
 -f 05_rabbitMq.yml \
 -f 06_zipkin.yml \
 -f 07_elk.yml \
+-f 08_promitheus.yml \
 up
