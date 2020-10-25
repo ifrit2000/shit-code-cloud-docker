e18474d666c2813614b2711b7affcb204a4b7493
Anthony <cdistc@163.com>
2020-10-25 22:24:19 +0800

    timezone

---
+++ docker-compose/cluster/script/stop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd $1
+cd ../compose-file
+docker-compose \
+-f 00_network.yml \
+-f 01_haproxy.yml \
+-f 02_consul.yml \
+-f 03_mysql.yml \
+-f 04_redis.yml \
+-f 05_rabbitMq.yml \
+-f 06_zipkin.yml \
+-f 07_elk.yml \
+-f 08_prometheus.yml \
+stop
+
+docker-compose \
+-f 00_network.yml \
+-f 10_admin.yml \
+-f 11_gateway.yml \
+stop
